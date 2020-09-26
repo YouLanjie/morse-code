32d4f17ea497596d91447118038e0a58e44f83f4
YouLanjie <66255345+YouLanjie@users.noreply.github.com>
2020-09-26 20:54:02 +0800

    Add files via upload

---
+++ head.h
@@ -0,0 +1,11 @@
+#include <stdio.h>                           //标准库
+#include <string.h>                          //strcmp();
+#include <stdlib.h>                          //system();srand();rand();
+#include <unistd.h>                          //sleep();
+#include <time.h>                            //time(NULL);
+
+void cls();
+void welcome();
+void morse1();
+void morse2();
+void help();
